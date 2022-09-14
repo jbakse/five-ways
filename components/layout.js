9fe8201794f6417458c8b30a9f3535fc600036d4
Justin Bakse <justin@tsfim.com>
2022-09-14 14:35:46 -0500

    update blogger name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Tammy Thunderstruck";
+const name = "Justin Bakse";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
