287b0f30c19619cb7d537d4a3d2681e1a9d1ce6e
Justin Bakse <justin@tsfim.com>
2022-09-22 20:31:21 -0500

    fix dependency warning in useAsyncDeep

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -4,26 +4,25 @@ import { useDeepCompareEffect } from "react-use";
 // useAsyncDeep adapted from
 // https://polvara.me/posts/fetching-asynchronous-data-with-react-hooks
 
-export function useAsyncDeep(getMethod, params) {
+export function useAsyncDeep(f, params) {
   const [value, setValue] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  async function getResource() {
-    try {
-      setLoading(true);
-      const result = await getMethod(...params);
-      setValue(result);
-    } catch (e) {
-      setError(e);
-    } finally {
-      setLoading(false);
-    }
-  }
 
   useDeepCompareEffect(() => {
+    async function getResource() {
+      try {
+        setLoading(true);
+        const result = await f(...params);
+        setValue(result);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    }
     getResource();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, params);
+  }, [f, params]);
 
   return { value, error, loading };
 }
