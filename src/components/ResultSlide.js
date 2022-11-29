90b2561e9c33dade95152a857b3a2e199d32ef24
Justin Bakse <justin@tsfim.com>
2022-11-29 13:18:15 -0600

    refactor result slide data/presentation

---
+++ src/components/ResultSlide.js
@@ -0,0 +1,32 @@
+import React, { useMemo } from "react";
+import { ShowJSON } from "./ShowJSON";
+import { PieChart } from "./PieChart";
+import styles from "./ResultSlide.module.scss";
+
+export function ResultSlide({ data }) {
+  // if (!data) return <></>;
+
+  //   data.options.sort((a, b) => b.count - a.count);
+  //   // assign letter after sorting
+  //   for (const [index, option] of data.options.entries()) {
+  //     option.label = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[index];
+  //   }
+
+  return (
+    <>
+      <div className={styles.Result}>
+        <div className={styles.QuestionWrap}>
+          <h1>{data.prompt}</h1>
+          {data.options.map((option) => (
+            <div key={option.index} className={styles.Option}>
+              {option.response}&nbsp;
+            </div>
+          ))}
+        </div>
+        <div className={styles.ChartWrap}>
+          <PieChart data={data.options.map((d) => d.count)} />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/results/[id].js
+++ src/pages/results/[id].js
@@ -1,48 +1,98 @@
+import unzip from "lodash/unzip";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useMemo } from "react";
 import { useAsync } from "react-use";
-import unzip from "lodash/unzip";
 import { Async } from "../../components/Async";
 import { ShowJSON } from "../../components/ShowJSON";
-import { PieChart } from "../../components/PieChart";
-import styles from "../../components/Result.module.scss";
+import { ResultSlide } from "../../components/ResultSlide";
+
+function useAsyncQuestion(questionId) {
+  return useAsync(async () => {
+    if (!questionId) return;
+    const response = await fetch(`/api/questions?id=${questionId}`);
+    const result = await response.json();
+    return result.question;
+  }, [questionId]);
+}
+
+function useAsyncResponses(questionId) {
+  return useAsync(async () => {
+    if (!questionId) return;
+
+    const response = await fetch(`/api/responses?questionId=${questionId}`);
+
+    const result = await response.json();
+    return result.responses;
+  }, [questionId]);
+}
+
+function useResultSummary(question, responses) {
+  return useMemo(
+    function updateData() {
+      if (responses.loading) return { loading: true, error: false };
+      if (responses.error) return { loading: false, error: responses.error };
+      if (question.loading) return { loading: true, error: false };
+      if (question.error) return { loading: false, error: question.error };
+
+      if (!Array.isArray(responses.value))
+        return { loading: true, error: false };
+      if (!question.value) return { loading: true, error: false };
+
+      // gather selections
+      const selections = responses.value.map((r) => r.selections);
+
+      // count how many respondants choose option 1, option 2, etc.
+      const selectionCounts = unzip(selections).map(
+        (a) => a.filter(Boolean).length
+      );
+
+      const viewedCount = responses.value.length;
+      const answeredCount = selectionCounts.reduce((a, b) => a + b, 0);
+
+      // prepare data
+      const options = selectionCounts.map((count, index) => ({
+        count,
+        index: index,
+        percentOfAnswered: count / answeredCount,
+        precentOfViewed: count / viewedCount,
+        response: question.value.optionTextsEnglish[index],
+      }));
+
+      const data = {
+        nickname: question.value.nickname,
+        prompt: question.value.promptTextEnglish,
+        viewedCount,
+        answeredCount,
+        options,
+      };
+
+      return data;
+    },
+    [responses, question]
+  );
+}
 
 export default function ResultPage(/*props*/) {
   const router = useRouter();
 
-  const question = useAsync(async () => {
-    if (!router.query.id) return;
-    const response = await fetch(`/api/questions?id=${router.query.id}`);
-    const result = await response.json();
-    return result.question;
-  }, [router.query.id]);
-
-  const responses = useAsync(async () => {
-    if (!router.query.id) return;
-
-    const response = await fetch(
-      `/api/responses?questionId=${router.query.id}`
-    );
-
-    const result = await response.json();
-    return result.responses;
-  }, [router.query.id]);
+  const question = useAsyncQuestion(router.query.id);
+  const responses = useAsyncResponses(router.query.id);
+  const summary = useResultSummary(question, responses);
 
   return (
     <>
       <Head>
         <title>Results</title>
       </Head>
-      <Async data={question}>
-        <Async data={responses}>
-          <Result
-            question={question.value}
-            responses={responses.value}
-          ></Result>
-        </Async>
+      <Async data={summary}>
+        <ResultSlide data={summary}></ResultSlide>
       </Async>
-
+      <Async data={summary}>
+        <ShowJSON title={`report_${question.value?.nickname}`}>
+          {summary}
+        </ShowJSON>
+      </Async>
       <Async className="content-block" data={question}>
         <ShowJSON title={`question_${question.value?.nickname}`}>
           {question.value}
@@ -53,75 +103,6 @@ export default function ResultPage(/*props*/) {
           {responses.value}
         </ShowJSON>
       </Async>
-    </>
-  );
-}
-
-function Result({ responses, question }) {
-  // const [data, setData] = useState(false);
-
-  const data = useMemo(
-    function updateData() {
-      if (!Array.isArray(responses)) return;
-      if (!question) return;
-
-      // gather selections
-      const selections = responses.map((r) => r.selections);
-
-      // count how many respondants choose option 1, option 2, etc.
-      const selectionCounts = unzip(selections).map(
-        (a) => a.filter(Boolean).length
-      );
-
-      const viewedCount = responses.length;
-      const answeredCount = selectionCounts.reduce((a, b) => a + b, 0);
-
-      // prepare data
-      const options = selectionCounts.map((count, index) => ({
-        count,
-        index: index,
-        percentOfAnswered: count / answeredCount,
-        precentOfViewed: count / viewedCount,
-        response: question.optionTextsEnglish[index],
-      }));
-
-      options.sort((a, b) => b.count - a.count);
-
-      for (const [index, option] of options.entries()) {
-        option.label = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[index];
-      }
-
-      const data = {
-        viewedCount,
-        answeredCount,
-        options,
-      };
-      return data;
-    },
-    [responses, question]
-  );
-
-  if (!data) return <></>;
-  return (
-    <>
-      <div className={styles.Result}>
-        <div className={styles.QuestionWrap}>
-          <h1>{question.promptTextEnglish}</h1>
-          {data.options.map((option) => (
-            <div key={option.index} className={styles.Option}>
-              {option.label}:&nbsp;
-              {option.response}&nbsp;
-              {/* {formatPercent(option.percentOfAnswered)}&nbsp; */}
-              {/* {formatPercent(option.precentOfViewed)} */}
-            </div>
-          ))}
-        </div>
-        <div className={styles.ChartWrap}>
-          <PieChart data={data.options.map((d) => d.count)} />
-        </div>
-      </div>
-
-      <ShowJSON title={`report_${question.nickname}`}>{data}</ShowJSON>
     </>
   );
 }
