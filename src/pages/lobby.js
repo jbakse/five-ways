3ecbe64113444061b956a1f7a472f337360bb889
Justin Bakse <justin@tsfim.com>
2022-12-02 16:32:26 -0600

    fix eslint errors

--- src/pages/lobby.js
+++ src/pages/lobby.js
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import Head from "next/head";
-import { v4 as uuidv4 } from "uuid";
 import { getSurveyDeep } from "../lib/airtable";
-import Question from "../components/Question";
 import SlideShow from "../components/SlideShow";
-import splash from "../components/Splash.module.scss";
-import LanguageSelect from "../components/LanguageSelect";
 
 export async function getServerSideProps(/*context*/) {
   const surveyId = "recVZKGGAb7BaWPvf"; //TODO: make configurable
